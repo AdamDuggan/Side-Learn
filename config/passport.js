38be93e82feaadb92774f5d8b2d9d7755653ffa9
AdamDuggan <adamduggan17@gmail.com>
2019-03-15 17:35:02 +0000

    start of video 32

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,33 @@ const User = mongoose.model('users');
 module.exports = function(passport){
     // Done is a callback 
     passport.use(new localStrategy({usernameField: 'email'}, (email, password, done) => {
-        // Start of video 31
-        // User.findOne({username: })
+        // Match User
+        User.findOne({email: email})
+        .then(user => {
+            if(!user){
+                return done(null, false, {message: 'No user found'});
+            }
+            // Match password: user.password is encrypted because it is coming from db 
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch){
+                    //Null coresponds with the first argument from callback so we want null error as first arg 
+                    return done(null, user);
+                }else{
+                    return done(null, false, {message: 'Password incorrect'});
+                }
+            })
+        })
     }))
+
+    // Serialise user
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
 }
