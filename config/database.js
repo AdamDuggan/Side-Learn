3b83342f9587e73b15ffad3f5521d0538705dd82
AdamDuggan <adamduggan17@gmail.com>
2019-03-18 11:09:01 +0000

    ready to push to heroku

---
+++ config/database.js
@@ -0,0 +1,6 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {mongoURI: 'mongodb://AdamJDuggan:Project2501@ds261155.mlab.com:61155/side-learn'}
+}
+else{
+    module.exports = {mongoURI: 'mongodb://AdamJDuggan:Project2501@ds261155.mlab.com:61155/side-learn'}
+}
